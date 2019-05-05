8c3058a438cf455a1ec095ffd7e12c7e2056f3a6
SimplyRin <simplyrin.dev@gmail.com>
2019-05-05 23:01:34 +0900

    add

--- src/main/java/net/simplyrin/socialdownloader/Controller.java
+++ src/main/java/net/simplyrin/socialdownloader/Controller.java
@@ -1,11 +1,21 @@
 package net.simplyrin.socialdownloader;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
@@ -13,7 +23,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import club.sk1er.utils.HttpClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -21,6 +30,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import net.simplyrin.httpclient.HttpClient;
+import net.simplyrin.multiprocess.MultiProcess;
 
 /**
  * Created by SimplyRin on 2018/10/11.
@@ -48,53 +59,57 @@ public class Controller {
 	@FXML
 	private TextField textField;
 
-	private String NANA_MUSIC = "nana-music.com";
-	private String LISPON = "lispon.moe";
-	private String TIKTOK = "tiktok.com";
-	private String TIKTOKV = "tiktokv.com";
-	private String LINE = "store.line.me/stickershop/product/";
-	private String TWITCH_CLIP = "clips.twitch.tv/";
+	private final String NANA_MUSIC = "nana-music.com";
+	private final String LISPON = "lispon.moe";
+	private final String TIKTOK = "tiktok.com";
+	private final String TIKTOKV = "tiktokv.com";
+	private final String LINE = "store.line.me/stickershop/product";
+	private final String TWITCH_CLIP = "clips.twitch.tv";
+	private final String PIXIV = "https://www.pixiv.net";
 
 	@FXML
 	private void onAction(ActionEvent event) {
-		String url = this.textField.getText();
+		String url = this.textField.getText().trim();
 		if (url.equals("")) {
 			this.buildAlert(Alert.AlertType.ERROR, "エラー", "URL を入力する必要があります。", null);
-			return;
-		}
-		if (!(url.contains(this.NANA_MUSIC) ||
-				url.contains(this.LISPON) ||
-				url.contains(this.TIKTOK) ||
-				url.contains(this.TIKTOKV) ||
-				url.contains(this.LINE) ||
-				url.contains(this.TWITCH_CLIP))) {
-			this.buildAlert(Alert.AlertType.ERROR, "エラー", "対応している URL を入力する必要があります。", "- nana-music.com\n- lispon.moe\n- tiktok.com\n- store.line.me\n- clips.twitch.tv");
 			return;
 		}
 
 		if (url.contains(this.NANA_MUSIC)) {
 			this.downloadNana(url);
+			return;
 		}
 
 		if (url.contains(this.LISPON)) {
 			this.downloadLispon(url);
+			return;
 		}
 
 		if (url.contains(this.TIKTOK) || url.contains(this.TIKTOKV)) {
 			this.downloadTikTok(url);
+			return;
 		}
 
 		if (url.contains(this.LINE)) {
 			this.downloadLineSticker(url);
+			return;
 		}
 
 		if (url.contains(this.TWITCH_CLIP)) {
 			this.downloadTwitchClip(url);
+			return;
 		}
+
+		if (url.contains(this.PIXIV)) {
+			this.downloadPixiv(url);
+			return;
+		}
+
+		this.buildAlert(Alert.AlertType.ERROR, "エラー", "このサービスは現在サポートされていません。", "- nana-music.com\n- lispon.moe\n- tiktok.com\n- store.line.me\n- clips.twitch.tv\n- pixiv.net");
 	}
 
 	private void downloadNana(String url) {
-		String content = HttpClient.rawWithAgent(url);
+		String content = HttpClient.fetch(url);
 		if (!content.contains("build_sound_url")) {
 			this.buildAlert(Alert.AlertType.ERROR, "エラー", "ファイルの検出に失敗しました。", null);
 			return;
@@ -148,7 +163,7 @@ public class Controller {
 		System.out.println("Detected qaId: " + qaId);
 		url = "http://lispon.moe/lispon/vaqa/getQAInfo?qaId=" + qaId;
 
-		String result = HttpClient.rawWithAgent(url);
+		String result = HttpClient.fetch(url);
 
 		JsonObject jsonObject;
 		try {
@@ -191,12 +206,12 @@ public class Controller {
 	}
 
 	private void downloadTikTok(String url) {
-		String content = HttpClient.rawWithAgent(url);
+		String content = HttpClient.fetch(url);
 		if(content.contains("Redirecting")) {
 			System.out.println("Redirecting to target link...");
 
 			String reconnectUrl = content.split("<a href=\"")[1].split("&amp;utm_source=copy_link")[0];
-			content = HttpClient.rawWithAgent(reconnectUrl);
+			content = HttpClient.fetch(reconnectUrl);
 		}
 
 		content = content.replace("\n", "");
@@ -208,7 +223,7 @@ public class Controller {
 		videoId = videoId.replace("\\u0026", "");
 
 		try {
-			HttpURLConnection connection = HttpClient.getHttpURLConnection("https://api.tiktokv.com/aweme/v1/playwm/?video_id=" + videoId);
+			HttpURLConnection connection = HttpClient.getHttpURLConnection("https://api.tiktokv.com/aweme/v1/playwm/?video_id=" + videoId, null, null);
 			url = connection.getHeaderField("Location");
 			if(url == null) {
 				new Exception();
@@ -237,7 +252,7 @@ public class Controller {
 	}
 
 	private void downloadLineSticker(String url) {
-		String content = HttpClient.rawWithAgent(url);
+		String content = HttpClient.fetch(url);
 
 		String title = content.split(" - LINE スタンプ | LINE STORE")[0].split("<title>")[1];
 		System.out.println("スタンプ名: " + title);
@@ -294,7 +309,7 @@ public class Controller {
 
 		url = "https://clips.twitch.tv/api/v2/clips/" + clipId + "/status";
 
-		String content = HttpClient.rawWithAgent(url);
+		String content = HttpClient.fetch(url);
 
 		JsonArray jsonArray = new JsonParser().parse(content).getAsJsonObject().get("quality_options").getAsJsonArray();
 		JsonObject jsonObject = jsonArray.get(0).getAsJsonObject();
@@ -316,6 +331,168 @@ public class Controller {
 			StackTrace stackTrace = new StackTrace();
 			e.printStackTrace(stackTrace);
 			this.buildAlert(Alert.AlertType.ERROR, "エラー", "ファイルのダウンロードに失敗しました。", "エラー:\n\n" + stackTrace.getContent());
+		}
+	}
+
+	private void downloadPixiv(String url) {
+		List<Integer> list = new ArrayList<>();
+
+		int memberId = 0;
+
+		if (url.contains("illust_id=")) {
+			int id;
+			try {
+				id = Integer.valueOf(url.split("illust_id=")[1]);
+			} catch (Exception e) {
+				this.buildAlert(Alert.AlertType.ERROR, "エラー", "リンクの解析に失敗しました。", null);
+				return;
+			}
+			list.add(id);
+			System.out.println("Detected: " + id);
+		} else if (url.replace("illust_id=", "").contains("id=")) {
+			try {
+				memberId = Integer.valueOf(url.split("id=")[1]);
+			} catch (Exception e) {
+				this.buildAlert(Alert.AlertType.ERROR, "エラー", "リンクの解析に失敗しました。", null);
+				return;
+			}
+			JsonObject jsonObject = HttpClient.getAsJsonObject("https://www.pixiv.net/ajax/user/" + memberId + "/profile/all");
+			JsonObject body = jsonObject.get("body").getAsJsonObject();
+			Set<String> keys = body.get("illusts").getAsJsonObject().keySet();
+
+			for (String key : keys) {
+				try {
+					list.add(Integer.valueOf(key));
+				} catch (Exception e) {
+				}
+			}
+		} else {
+			this.buildAlert(Alert.AlertType.ERROR, "エラー", "イラストIDまたはメンバーIDのURLを貼り付けてください。", null);
+			return;
+		}
+
+		File directory = this.openDirectoryChooser("保存場所を選択してダウンロード");
+		if (directory == null) {
+			this.buildAlert(Alert.AlertType.ERROR, "エラー", "保存場所の取得に失敗しました", null);
+			return;
+		}
+
+		if (list.size() != 1) {
+			directory = new File(directory, "pixiv_" + memberId);
+		}
+
+		List<Integer> failedDlImages = new ArrayList<>();
+
+		File pixiv = new File("pixiv.json");
+
+		boolean useProxy = false;
+		File proxyFile = null;
+		List<String> proxies = null;
+		int uses = 0;
+
+		if (pixiv.exists()) {
+			JsonObject jsonObject = new JsonParser().parse(this.readAll(pixiv)).getAsJsonObject();
+
+			useProxy = jsonObject.get("useProxy").getAsBoolean();
+			if (useProxy) {
+				proxyFile = new File(jsonObject.get("file").getAsString());
+				if (proxyFile.exists()) {
+					proxies = this.readLines(proxyFile);
+				}
+			}
+		}
+
+		MultiProcess multiProcess = new MultiProcess();
+
+		for (Integer key : list) {
+			HttpClient httpClient = new HttpClient("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + key.intValue());
+			httpClient.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36");
+			String result = httpClient.getResult();
+
+			result = result.replace("\"", "");
+			result = result.replace("\\/", "/");
+
+			result = result.replace(",", "");
+
+			String imageUrl = null;
+			for (String imgUrl : result.split("<img src=")) {
+				imgUrl = imgUrl.split(" ")[0];
+
+				if (imgUrl.contains(key + "_p0_")) {
+					imageUrl = imgUrl;
+				}
+			}
+
+			// https://i.pximg.net/c/600x600/img-master/img/2019/05/02/00/00/06/74502133_p0_master1200.jpg
+			imageUrl = imageUrl.split("/img-master/")[1].split("_p0_")[0];
+			imageUrl = "https://i.pximg.net/img-original/" + imageUrl + "_p0.png";
+
+			System.out.println("Img: " + imageUrl);
+
+			File file = new File(directory, key + ".png");
+
+			final String u = imageUrl;
+			final boolean us = useProxy;
+			final List<String> pro = proxies;
+
+			int added = 0;
+			while (true) {
+				if (added == 10) {
+					break;
+				}
+				final int adf = added;
+				multiProcess.addProcess(() -> this.dlTest(httpClient, u, us, pro, adf, file));
+				added++;
+			}
+
+			/* try {
+				httpClient = new HttpClient(imageUrl);
+				httpClient.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36");
+
+				if (useProxy) {
+					String proxy = proxies.get(uses);
+					System.out.println("with Proxy: " + proxy);
+					httpClient.setProxy(new Proxy(Type.HTTP, new InetSocketAddress(proxy.split(":")[0], Integer.valueOf(proxy.split(":")[1]))));
+					uses++;
+				}
+
+				FileUtils.copyInputStreamToFile(httpClient.getInputStream(), file);
+			} catch (Exception e) {
+				// e.printStackTrace();
+				// failedDlImages.add(key.intValue());
+			} */
+		}
+
+		multiProcess.updateMaxThread(32);
+		multiProcess.setFinishedTask(() -> this.buildAlert(Alert.AlertType.INFORMATION, "完了", "ファイルのダウンロードが完了しました。", "ダウンロードできなかったファイル:\n\n" + failedDlImages.toString()) );
+		multiProcess.start();
+
+		if (failedDlImages.size() > 0) {
+			//this.buildAlert(Alert.AlertType.ERROR, "エラー", "ファイルのダウンロードに失敗しました。", );
+			return;
+		} else {
+			//this.buildAlert(Alert.AlertType.INFORMATION, "完了", "ファイルのダウンロードが完了しました。", null);
+		}
+	}
+
+	private void dlTest(HttpClient httpClient, String imageUrl, boolean useProxy, List<String> proxies, int uses, File file) {
+		try {
+			httpClient = new HttpClient(imageUrl);
+			httpClient.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36");
+
+			if (useProxy) {
+				String proxy = proxies.get(uses);
+				System.out.println("with Proxy: " + proxy);
+				httpClient.setProxy(new Proxy(Type.HTTP, new InetSocketAddress(proxy.split(":")[0], Integer.valueOf(proxy.split(":")[1]))));
+				uses++;
+			}
+
+			FileUtils.copyInputStreamToFile(httpClient.getInputStream(), file);
+		} catch (Exception e) {
+			System.out.println("Failed. continue...");
+			this.dlTest(httpClient, imageUrl, useProxy, proxies, uses, file);
+			// e.printStackTrace();
+			// failedDlImages.add(key.intValue());
 		}
 	}
 
@@ -345,6 +522,35 @@ public class Controller {
 		directoryChooser.setTitle(title);
 
 		return directoryChooser.showDialog(this.button.getScene().getWindow());
+	}
+
+	/**
+	 * Created by @penguinshunya
+	 * URL: https://qiita.com/penguinshunya/items/353bb1c555f337b0cf6d
+	 */
+	private String readAll(File file) {
+		try {
+			return Files.lines(Paths.get(file.getPath()), Charset.forName("UTF-8"))
+					.collect(Collectors.joining(System.getProperty("line.separator")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private List<String> readLines(File file) {
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+		} catch (Exception e) {
+		}
+
+		List<String> list = new ArrayList<>();
+		while (scanner.hasNext()) {
+			list.add(scanner.nextLine());
+		}
+		scanner.close();
+		return list;
 	}
 
 }
